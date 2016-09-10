3ee5c3570b732a7ef6828c54638c0e1baf9621c1
James Gilmore <james.gilmore@red-gate.com>
2016-09-10 11:45:28 +0100

    Enable/disable based on TAP content of page

--- bg.js
+++ bg.js
@@ -1,21 +1,43 @@
 var tapDocuments = new Set();
 
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+    // The page can send us messages if the extension loads inside it and finds TAP.
     if(request.msg == "TAP_START") {
-	tapDocuments.add(sender.tab.url);
+	enableUI();
+	tapDocuments.add(sender.tab.id);
     } else {
-	tapDocuments.delete(sender.tab.url);
+	disableUI();
+	tapDocuments.delete(sender.tab.id);
     }
     
     sendResponse({msg : "OK"});
 });
 
-function callMethodIfTapPresent(callback) {
-    chrome.tabs.query({active:true,lastFocusedWindow:true},
-		      function(tabs) {
-			  var url = tabs[0].url;
-			  if(tapDocuments.has(url)) {
-			      callback();
-			  }
-		      });
+chrome.tabs.onActivated.addListener(function(activeInfo) {
+    // When the user changes tabs, see if it's a tap we know about already.
+    if(tapDocuments.has(activeInfo.tabId)) {
+	enableUI();
+    } else {
+	disableUI();
+    }  
+});
+
+chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
+    // Assume that changing URL is not going to take us to a TAP page. If it does, then
+    // TAP_START will be sent from the page, enabling the icon again.
+    //
+    // Likely this will not work if a non-active tab changes URL while we're not looking at it.
+    disableUI();
+});
+
+function enableUI() {
+    chrome.browserAction.setIcon({path : "icon-live.png"});
+    chrome.browserAction.setPopup({popup : "popup.html"});
 }
+
+function disableUI() {
+    chrome.browserAction.setIcon({path : "icon.png"});
+    chrome.browserAction.setPopup({popup : ""});
+}
+
+
--- parse-tap.js
+++ parse-tap.js
@@ -1,9 +1,6 @@
 document.addEventListener('DOMContentLoaded', function() {
     var button = document.getElementById('parse-button');
-    
+
     button.onclick = function() { alert("Clicked"); };
-    chrome.extension.getBackgroundPage().callMethodIfTapPresent(function() {
-	alert("Foo");
-    });
 });
 
