4dbe7e217bbb9476b67c53ed8511125c3339904e
James Gilmore <james.gilmore@red-gate.com>
2016-09-10 12:31:42 +0100

    Copy the TAP data into a div

--- content-script.js
+++ content-script.js
@@ -1,4 +1,4 @@
-var data = document.querySelector("pre").innerHTML;
+var data = $("pre")[0].innerHTML;
 var port = chrome.runtime.connect({ name : "taptaptap"});
 
 function reportTapStatus(message) {
@@ -9,8 +9,15 @@ function reportTapStatus(message) {
 			       });
 }
 
-if(data.startsWith("TAP")) {
+if(data != null && data.startsWith("TAP")) {
     reportTapStatus("TAP_START");
 } else {
     reportTapStatus("TAP_END");
 }
+
+$div = $("body").append("<div></div>");
+$div.css({"font-family" : "monospace"});
+for(let line of data.split("\n")) {
+    $div.append(line+"</br>");
+}
+
