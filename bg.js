af6fd55d7db13d81a02d8e39c1601479eeaefbc0
James Gilmore <james.gilmore@red-gate.com>
2016-09-13 18:25:34 +0100

    Don't send message if no active tab

--- bg.js
+++ bg.js
@@ -36,7 +36,11 @@ function disableUI(tabId) {
 
 function sendMessage(message) {
     chrome.tabs.query({active:true,currentWindow:true}, function(tabs) {
-	chrome.tabs.sendMessage(tabs[0].id, {msg : message}, null);
+	if(tabs.length > 0) {
+	    chrome.tabs.sendMessage(tabs[0].id, {msg : message}, null);
+	} else {
+	    console.log("No active tabs, not sending " + message);
+	}
     });
 }
 
