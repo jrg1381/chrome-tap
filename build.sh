907b02a21017133e4a3c09aff2362bef0bff9a33
James Gilmore <james.gilmore@red-gate.com>
2016-09-17 12:38:47 +0100

    Write the data to the parser
    Add some example events to fire when comments etc are encountered. Not
    getting the 'completed' event..

--- build.sh
+++ build.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
 
+if [ -z $(which browserify) ]; then
+    echo "browserify not on path, aborting"
+    exit 1
+fi
+
 npm install
 browserify content-script.js -o content-script-bundle.js
--- content-script.js
+++ content-script.js
@@ -1,4 +1,4 @@
-var dummy = require('tap-parser');
+var parser = require('tap-parser');
 
 var passesHidden = false;
 var showingParsedTap = true;
@@ -7,9 +7,9 @@ var invisibleClass = "chrome-tap-invisible";
 function reportTapStatus(message) {
     console.log(message);
     chrome.runtime.sendMessage({msg : message},
-			       function(response) {
-				   console.log(response.msg);
-			       });
+                               function(response) {
+                                   console.log(response.msg);
+                               });
 }
 
 function spanWithClass(contents, spanClass) {
@@ -18,20 +18,20 @@ function spanWithClass(contents, spanClass) {
 
 function tapSwitchView(preNode) {
     if(showingParsedTap) {
-	preNode.removeClass(invisibleClass);
-	$("#chrome-tap-parsed-output").addClass(invisibleClass);
+        preNode.removeClass(invisibleClass);
+        $("#chrome-tap-parsed-output").addClass(invisibleClass);
     } else {
-	preNode.addClass(invisibleClass);
-	$("#chrome-tap-parsed-output").removeClass(invisibleClass);
+        preNode.addClass(invisibleClass);
+        $("#chrome-tap-parsed-output").removeClass(invisibleClass);
     }
     showingParsedTap = !showingParsedTap;
 }
 
 function tapHidePasses() {
     if(passesHidden) {
-	$(".chrome-tap-ok").removeClass(invisibleClass);
+        $(".chrome-tap-ok").removeClass(invisibleClass);
     } else {
-	$(".chrome-tap-ok").addClass(invisibleClass);
+        $(".chrome-tap-ok").addClass(invisibleClass);
     }
     passesHidden = !passesHidden;
 }
@@ -41,24 +41,24 @@ $(document).ready(function() {
     var preNode = $("pre")[0];
     
     chrome.runtime.onMessage.addListener(
-	function(request, sender, sendResponse) {
-	    switch(request.msg) {
-	    case "TAP_SWITCH_VIEW":
-		tapSwitchView($(preNode));
-		break;
-	    case "TAP_SWITCH_HIDE_PASSES":
-		tapHidePasses();
-		break;
-	    }
-	});
+        function(request, sender, sendResponse) {
+            switch(request.msg) {
+            case "TAP_SWITCH_VIEW":
+                tapSwitchView($(preNode));
+                break;
+            case "TAP_SWITCH_HIDE_PASSES":
+                tapHidePasses();
+                break;
+            }
+        });
     
     var data = preNode.innerHTML;
 
     if(data != null && /1\.\.\d+/.test(data)) {
-	reportTapStatus("TAP_START");
+        reportTapStatus("TAP_START");
     } else {
-	reportTapStatus("TAP_END");
-	return;
+        reportTapStatus("TAP_END");
+        return;
     }
 
     $(preNode).removeAttr('style');
@@ -71,16 +71,26 @@ $(document).ready(function() {
     $("body").append($div);
     $div.addClass("chrome-tap-pre");
     for(let line of data.split("\n")) {
-	if(/^\s*#/.test(line)) {
-	    line = spanWithClass(line, "chrome-tap-comment");
-	} else if(/^\s*ok/.test(line)) {
-	    line = spanWithClass(line, "chrome-tap-ok");
-	} else if(/^\s*not ok/.test(line)) {
-	    line = spanWithClass(line, "chrome-tap-not-ok");
-	} else {
-	    line = spanWithClass(line, "chrome-tap-default");
-	}
-	$div.append(line);
+        if(/^\s*#/.test(line)) {
+            line = spanWithClass(line, "chrome-tap-comment");
+        } else if(/^\s*ok/.test(line)) {
+            line = spanWithClass(line, "chrome-tap-ok");
+        } else if(/^\s*not ok/.test(line)) {
+            line = spanWithClass(line, "chrome-tap-not-ok");
+        } else {
+            line = spanWithClass(line, "chrome-tap-default");
+        }
+        $div.append(line);
     }
+
+    var p = parser(function(results) {
+        console.log(results);
+    });
+    
+    p.on('comment', function(comment) {
+        console.log(comment);
+    });
+
+    p.write(data);
 });
 
