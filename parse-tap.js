f1917d6a36ea76f66835a710dea9185a19d31b98
James Gilmore <jamesg1381@gmail.com>
2016-10-25 20:21:27 +0100

    Fix issue #9 - use newer API

--- parse-tap.js
+++ parse-tap.js
@@ -3,11 +3,11 @@ var SHOW_RAW = "Show raw TAP";
 var backgroundPage;
 
 document.addEventListener('DOMContentLoaded', function() {
-    backgroundPage = chrome.extension.getBackgroundPage();
+    chrome.runtime.getBackgroundPage(function(backgroundPage) {
+        var viewButton = document.getElementById('parse-button');
+        viewButton.onclick = backgroundPage.switchView;
+    });
     
-    var viewButton = document.getElementById('parse-button');
-    viewButton.onclick = backgroundPage.switchView;
-
     var optionsButton = document.getElementById('options');
     optionsButton.onclick = function() {
         chrome.runtime.openOptionsPage();
