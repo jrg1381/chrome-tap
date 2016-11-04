2d54235e12cd80b9c34e8c37c5d13e0956239812
James Gilmore <james.gilmore@red-gate.com>
2016-11-04 21:32:11 +0000

    Update build script to check for phantomjs and phantom-jasmine

--- build.sh
+++ build.sh
@@ -17,9 +17,15 @@ else
     rm build/*
 fi
 
+if [ ! -z $(which phantomjs) -a ! -z $(which phantom-jasmine) ]; then
+    echo "Tests will not run"
+fi
+
 cp manifest.json LICENSE *.js *.css *.html *.png build
 # Because it's already included in CtAppBundle.js
 rm build/CtApp.js
+# Don't need the gruntfile in there
+rm build/Gruntfile.js
 
 rm -f build/chrome-tap.zip
 # -j => junk the paths
